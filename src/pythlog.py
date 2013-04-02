73e7e02a07ddcaa555fe32f9d148cd49dc2e4797
Torgny Andersson <torgny.andersson@gmail.com>
2013-04-03 01:15:27 +0200

    Added support for many int-operations and if-statements.

--- src/pythlog.py
+++ src/pythlog.py
@@ -28,6 +28,9 @@ class Allocator:
     def tempvar(self):
         return "T%s" % self._alloc_number()
 
+    def globalsym(self):
+        return "g_%s" % self._alloc_number()
+
 class ModuleTranslator(ast.NodeVisitor):
     def __init__(self, global_symbols):
         self._global_symbols = global_symbols
@@ -41,8 +44,7 @@ class ModuleTranslator(ast.NodeVisitor):
         ft = FunctionTranslator(node,
                                self._allocator,
                                self._global_symbols)
-        code = ft.predicates()
-        self._predicates.append(code)
+        self._predicates.extend(ft.predicates())
 
     def visit_ClassDef(self, node):
         ct = ClassTranslator(node,
@@ -56,6 +58,15 @@ class StatementTranslator(ast.NodeVisitor):
         self._allocator = allocator
         self._globals = globals
         self._code = []
+        self._predicates = []
+
+    def code(self):
+        return self._code
+
+    def predicates(self):
+        """Get any additional predicates created while translating the
+        statement (needed for ifs, loops, etc)."""
+        return self._predicates
 
     # Expression nodes
     def visit_Name(self, node):
@@ -64,12 +75,20 @@ class StatementTranslator(ast.NodeVisitor):
     def visit_Num(self, node):
         return "t_int(%s)" % node.n
 
+    def visit_UnaryOp(self, node):
+        operand = self.visit(node.operand)
+        op = node.op.__class__.__name__.lower()
+        result = self._allocator.tempvar()
+        self._code.append('i_unary%s(%s, %s)' % (op, operand, result))
+        return result
+
+
     def visit_BinOp(self, node):
         lhs = self.visit(node.left)
         rhs = self.visit(node.right)
         op = node.op.__class__.__name__.lower()
         result = self._allocator.tempvar()
-        self._code.append('i_binop(%s, %s, %s, %s)' % (op, lhs, rhs, result))
+        self._code.append('i_bin%s(%s, %s, %s)' % (op, lhs, rhs, result))
         return result
 
     def visit_Compare(self, node):
@@ -84,13 +103,8 @@ class StatementTranslator(ast.NodeVisitor):
 
     def visit_Call(self, node):
         result = self._allocator.tempvar()
-        if node.func.id == 'print':
-            func = 'i_print'
-        else:
-            func = node.func.id
-
         args = ", ".join(self.visit(a) for a in node.args)
-        self._code.append("%s([%s], Io, %s)" % (func, args, result))
+        self._code.append("%s([%s], Io, %s)" % (node.func.id, args, result))
         return result
 
     # Statement nodes
@@ -116,52 +130,150 @@ class StatementTranslator(ast.NodeVisitor):
         self.visit(node.value)
         return self._code
 
+    def visit_If(self, node):
+        test = self.visit(node.test)
+        body = []
+        for stmt in node.body:
+            st = StatementTranslator(self._allocator, self._globals)
+            body.extend(st.visit(stmt))
+        orelse = []
+        for stmt in node.orelse:
+            st = StatementTranslator(self._allocator, self._globals)
+            orelse.extend(st.visit(stmt))
+
+        name = self._allocator.globalsym()
+        args = node.invars + node.outvars + ["Io"]
+
+        self._predicates.append(generate_predicate(name,
+                                                   ['t_bool(1)'] + args,
+                                                   body))
+        self._predicates.append(generate_predicate(name,
+                                                   ['t_bool(0)'] + args,
+                                                   orelse))
+        self._code.append('%s(%s, %s)' % (name, test, ", ".join(args)))
+        return self._code
+
+def generate_predicate(name, args, body):
+    if len(body) == 0:
+        suffix = "."
+    else:
+        suffix = " :-\n  " + ",\n  ".join(body) + "."
+    return "%s(%s)%s" % (name, ", ".join(args), suffix)
+
 class FunctionTranslator(ast.NodeVisitor):
     def __init__(self, node, allocator, globals):
         self._node = node
         self._allocator = allocator
         self._globals = globals
         self._code = []
+        self._predicates = []
 
     def predicates(self):
         for stmt in self._node.body:
             st = StatementTranslator(self._allocator, self._globals)
 #            print(ast.dump(stmt))
-            self._code.extend(st.visit(stmt))
+            st.visit(stmt)
+            self._code.extend(st.code())
+            self._predicates.extend(st.predicates())
 
-        args = ", ".join("L%s0" % a.arg for a in self._node.args.args)
-        head = "'%s'([%s], Io, Result) :-\n  " % (self._node.name, args)
-        return head + ",\n  ".join(self._code) + "."
-
+        func_args = "[%s]" % (", ".join(a.arg for a in self._node.args.args))
+        pred_args = [func_args, 'Io', 'Result']
+        self._predicates.append(generate_predicate(self._node.name,
+                                                   pred_args,
+                                                   self._code))
+        return self._predicates
 
 class SsaRewriter(ast.NodeTransformer):
     def __init__(self):
         self._locals = {}
 
+    def _localvar_index(self, id, locs):
+        return int(locs[id][len(id) + 1:])
+
+    def _localvar_name(self, id, idx):
+        return "L%s%s" % (id, idx)
+
     def _new_local(self, id):
         if id in self._locals:
-            index = int(self._locals[id][len(id) + 1:]) + 1
+            index = self._localvar_index(id, self._locals) + 1
         else:
             index = 0
-        next_id = "L%s%s" % (id, index)
+        next_id = self._localvar_name(id, index)
         self._locals[id] = next_id
         return next_id
 
+    def visit_arg(self, node):
+        newarg = self._new_local(node.arg)
+        return ast.copy_location(ast.arg(arg=newarg,
+                                         annotation=node.annotation),
+                                 node)
+
     def visit_FunctionDef(self, node):
-        self._locals = {}
-        for a in node.args.args:
-            self._new_local(a.arg)
-        return self.generic_visit(node)
+        """
+        Makes the name of functions not clash with Prolog names by prefixing
+        the function name with 'g_' (read: global_). 
+        """
+        self._locals = {} # New function started => clear locals
+        args = self.visit(node.args) # Must be visited before the body
+        body = [self.visit(stmt) for stmt in node.body]
+        new = ast.FunctionDef(name="g_" + node.name,
+                              args=args,
+                              body=body)
+        return ast.copy_location(new, node)
+
+    def visit_If(self, node):
+        """
+        Canonifies if statemenets such that if the then-body assignes a local,
+        so does the else-body (by simply doing a copy if necessary). Furthermore,
+        this function adds a field to the if-node called 'outvars', which is the
+        set of variables assigned by this node.
+        """
+        test = self.visit(node.test)
+        locals_before = self._locals.copy()
+        body = [self.visit(stmt) for stmt in node.body]
+        locals_after_body = self._locals.copy()
+        self._locals = locals_before.copy()
+        orelse = [self.visit(stmt) for stmt in node.orelse]
+        locals_after_orelse = self._locals.copy()
+
+        all_vars = set(locals_after_body.keys()).union(set(locals_after_orelse.keys()))
+        self._locals = locals_before.copy()
+        outvars = []
+        for var in all_vars:
+            if (var not in locals_before and
+                var in locals_after_body and
+                var in locals_after_body): # Var added in both branches
+                body_idx = self._localvar_index(var, locals_after_body)
+                orelse_idx = self._localvar_index(var, locals_after_orelse)
+                if body_idx > orelse_idx:
+                    max_idx, not_max_idx = body_idx, orelse_idx
+                else:
+                    max_idx, not_max_idx = orelse_idx, body_idx
+                out_var = self._localvar_name(var, max_idx)
+                in_var = self._localvar_name(var, not_max_idx)
+                assign = ast.Assign(targets=[ast.Name(id=out_var, ctx=ast.Store())],
+                                    value=ast.Name(id=in_var, ctx=ast.Load()))
+                if max_idx > body_idx:
+                    body.append(assign)
+                elif max_idx > orelse_idx:
+                    orelse.append(assign)
+                self._locals[var] = out_var
+                outvars.append(out_var)
+        new = ast.If(test=test, body=body, orelse=orelse)
+        new._fields = new._fields + ('outvars', 'invars')
+        new.outvars = outvars
+        new.invars = list(locals_before.values())
+        return ast.copy_location(new, node)
 
     def visit_Name(self, node):
         if node.id == 'free':
-            id = '_'
+            id = self._new_local(node.id)
         elif node.ctx.__class__ == ast.Store:
             id = self._new_local(node.id)
         elif node.id in self._locals:
             id = self._locals[node.id]
         else:
-            return node # Not a local var
+            id = "g_" + node.id # Not a local var => global name
         new = ast.Name(id=id, ctx=node.ctx)
         return ast.copy_location(new, node)
 
@@ -183,10 +295,10 @@ def global_symbols(parse_tree):
 def compile_module(module_code):
     parse_tree = ast.parse(module_code)
     ssa_tree = ssa_form(parse_tree)
+
     compiler = ModuleTranslator(global_symbols(ssa_tree))
     compiler.visit(ssa_tree)
     return BUILTINS + compiler.code()
-
 
 def main():
     import argparse
@@ -206,6 +318,19 @@ def main():
     outfile.close()
 
 
+
+def int_and_body():
+    idxs = range(0, 64) # Bits
+    l_bits = ", ".join("L%s" % b for b in idxs)
+    r_bits = ", ".join("R%s" % b for b in idxs)
+    bits = "[%s, %s] ins 0..1" % (l_bits, r_bits)
+    l = "Ls #= " + " + ".join("L%s * %s" % (b, 2**b) for b in idxs)
+    r = "Rs #= " + " + ".join("R%s * %s" % (b, 2**b) for b in idxs)
+    result = "Result #= " + " + ".join("L%s * R%s * %s" % (b, b, 2**b) for b in idxs)
+    return ",\n    ".join([
+            'no_sign(L, Ls), no_sign(R, Rs)',
+            bits, l, r, result])
+
 BUILTINS = """
 ?- use_module(library(clpfd)).
 
@@ -213,15 +338,48 @@ i_assign(Var, Var).
 
 i_assert(t_bool(1)).
 
-i_binop(add, t_int(L), t_int(R), t_int(Result)) :-
-    Result #= L + R.
-i_binop(sub, t_int(L), t_int(R), t_int(Result)) :-
-    Result #= L - R.
-i_binop(mult, t_int(L), t_int(R), t_int(Result)) :-
-    Result #= L * R.
-i_binop(div, t_int(L), t_int(R), t_int(Result)) :-
-    Result #= L / R.
+% TODO: These cuts should not be here.
+i_binadd(L, R, Result) :-
+    m___add__(L, [R], _, Result), !.
+i_binadd(L, R, Result) :-
+    m___radd__(R, [L], _, Result), !.
+i_binsub(L, R, Result) :-
+    m___sub__(L, [R], _, Result), !.
+i_binsub(L, R, Result) :-
+    m___rsub__(R, [L], _, Result), !.
+i_binmult(L, R, Result) :-
+    m___mult__(L, [R], _, Result), !.
+i_binmult(L, R, Result) :-
+    m___rmult__(R, [L], _, Result), !.
+i_bindiv(L, R, Result) :-
+    m___div__(L, [R], _, Result), !.
+i_bindiv(L, R, Result) :-
+    m___rdiv__(R, [L], _, Result), !.
+i_binmod(L, R, Result) :-
+    m___mod__(L, [R], _, Result), !.
+i_binmod(L, R, Result) :-
+    m___rmod__(R, [L], _, Result), !.
+i_binpow(L, R, Result) :-
+    m___pow__(L, [R], _, Result), !.
+i_binpow(L, R, Result) :-
+    m___rpow__(R, [L], _, Result), !.
+i_binrshift(L, R, Result) :-
+    m___rshift__(L, [R], _, Result), !.
+i_binrshift(L, R, Result) :-
+    m___rrshift__(R, [L], _, Result), !.
+i_binlshift(L, R, Result) :-
+    m___lshift__(L, [R], _, Result), !.
+i_binlshift(L, R, Result) :-
+    m___rlshift__(R, [L], _, Result), !.
+i_binbitand(L, R, Result) :-
+    m___and__(L, [R], _, Result), !.
+i_binbitand(L, R, Result) :-
+    m___rand__(R, [L], _, Result), !.
 
+i_unaryusub(I, Result) :-
+    m___neg__(I, [], _, Result).
+i_unaryinvert(I, Result) :-
+    m___invert__(I, [], _, Result).
 
 i_compare(ne, t_int(L), t_int(R), t_bool(Result)) :-
     Result #<==> (L #\= R).
@@ -231,33 +389,65 @@ i_compare(lt, t_int(L), t_int(R), t_bool(Result)) :-
     Result #<==> (L #< R).
 i_compare(gt, t_int(L), t_int(R), t_bool(Result)) :-
     Result #<==> (L #> R).
-i_compare(ge, t_int(L), t_int(R), t_bool(Result)) :-
+i_compare(gte, t_int(L), t_int(R), t_bool(Result)) :-
     Result #<==> (L #>= R).
-i_compare(le, t_int(L), t_int(R), t_bool(Result)) :-
+i_compare(lte, t_int(L), t_int(R), t_bool(Result)) :-
     Result #<==> (L #=< R).
 
-i_print(Objects, Io, t_None) :-
+i_return(Var, Var).
+
+
+m___add__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L + R.
+m___sub__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L - R.
+m___mult__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L * R.
+m___div__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L / R.
+m___mod__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    L #< 0, R #< 0, !,
+    Result #= -(-L mod -R).
+m___mod__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L mod R.
+m___pow__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L ^ R.
+m___rshift__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L / (2 ^ R).
+m___lshift__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    Result #= L * (2 ^ R).
+m___and__(t_int(L), [t_int(R)], _Io, t_int(Result)) :-
+    {int_and_body}.
+m___neg__(t_int(I), [], _Io, t_int(-I)).
+m___invert__(t_int(I), [], _Io, t_int(Result)) :-
+    Result #= -I -1.
+
+
+no_sign(I, Is) :-
+    I #< 0, !,
+    Is #= {max_bit_val} + I.
+no_sign(I, I).
+
+to_print_string([], Acc, t_str(Acc)).
+to_print_string([H|T], Acc, Result) :-
+    g_str(H, t_str(HStr)),
+    append(Acc, HStr, NextAcc),
+    to_print_string(T, NextAcc, Result).
+
+
+g_print(Objects, Io, t_None) :-
     to_print_string(Objects, [], Str),
     io(List) = Io,
     append(List, [Str], Result),
     setarg(1, Io, Result).
 
-i_return(Var, Var).
-
-
-to_print_string([], Acc, t_str(Acc)).
-to_print_string([H|T], Acc, Result) :-
-    f_str(H, t_str(HStr)),
-    append(Acc, HStr, NextAcc),
-    to_print_string(T, NextAcc, Result).
-
-f_repr(t_int(I), t_str(Repr)) :-
+g_repr(t_int(I), t_str(Repr)) :-
     integer(I), !,
     number_codes(I, Repr).
-f_repr(t_int(_), t_str("?int")).
+g_repr(t_int(_), t_str("?int")).
 
-f_str(Object, Str) :-
-    f_repr(Object, Str).
+g_str(Object, Str) :-
+    g_repr(Object, Str).
 
 
 io_write([]).
@@ -268,7 +458,7 @@ io_write([H|T]) :-
     io_write(T).
 
 start :-
-    main([], Io, _) -> 
+    g_main([], Io, _) -> 
      (io(Printed) = Io, io_write(Printed))
      ; (write('Goal ''main'' failed.'), nl)
     ,
@@ -277,8 +467,7 @@ start :-
 :- style_check(-singleton).
 :- style_check(-discontiguous).
 
-"""
-
+""".format(int_and_body=int_and_body(), max_bit_val=2**64)
 
 if __name__ == '__main__':
     main()
--- test2/int_test.py
+++ test2/int_test.py
@@ -1,8 +1,97 @@
 
-def test_int_add(a, b):
-    c = free
-    assert a + b * c == a
-    print(c)
+def int_test0_add():
+    assert 1 + 2 == 3
+    assert 6 + 9 == 15
+    x = free
+    assert x + 8 == 28
+    assert x == 20
+
+def int_test1_sub():
+    assert 7 - 2 == 5
+    x = free
+    assert x - 8 == 20
+    assert x == 28
+
+def int_test2_mult():
+    assert 7 * 2 == 14
+    x = free
+    assert x * 8 == 48
+    assert x == 6
+
+def int_test3_div():
+    assert 6 / 2 == 3
+    x = free
+    assert x / 6 == 7
+    assert x == 42
+
+def int_test4_mod():
+    assert 5 % 2 == 1
+    assert -5 % 2 == 1
+    assert 5 % -2 == -1
+    assert -5 % -2 == -1
+
+    x = free
+    assert x % 2 == 1
+    assert x == 3
+
+def int_test5_exp():
+    assert 2 ** 10 == 1024
+
+    x = free
+    assert x ** 10 == 1024
+    assert x == 2
+
+    y = free
+    assert 2 ** y == 1024
+    assert y == 10
+
+def int_test6_shift():
+    assert 6 >> 1 == 3
+    assert -82 << 2 == -328
+
+    x = free
+    assert x << 8 == 512
+    assert x == 2
+
+    y = free
+    assert 2 << y == 128
+    assert y == 6
+
+def int_test7_bit_inverse():
+    assert ~67 == -68
+    assert ~-76 == 75
+
+    x = free
+    assert ~x == 99
+    assert x == -100
+
+def int_test8_bit_and():
+    assert 64372483 & 473982479238 == 59124482
+    assert -749382 & 43792 == 32784
+
+def fac(n):
+    assert n >= 0
+    if n == 0:
+        f = 1
+    else:
+        f = fac(n - 1) * n
+    return f
+
+def int_test10_factorial():
+    assert fac(7) == 5040
+    n = free
+    assert 5040 == fac(n)
+    assert n == 7
 
 def main():
-    test_int_add(150, 4)
+    int_test0_add()
+    int_test1_sub()
+    int_test2_mult()
+    int_test3_div()
+    int_test4_mod()
+    int_test5_exp()
+    int_test6_shift()
+    int_test7_bit_inverse()
+    int_test8_bit_and()
+    int_test10_factorial()
+    print(777)
