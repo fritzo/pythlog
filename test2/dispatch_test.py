dcb888d18b0e91e82511d3e11d1f535a1b716ec2
Torgny Andersson <torgny.andersson@gmail.com>
2013-05-02 20:43:28 +0200

    Added tests of matching values to function arguments.

--- test2/dispatch_test.py
+++ test2/dispatch_test.py
@@ -65,8 +65,43 @@ def dispatch_test2_match_in_arg_list():
     assert func2(2, 1) == 2
     assert func2(1, 1) == 3
 
+
+def out_argument(x):
+    assert x == 10
+
+def dispatch_test3_out_argument():
+    x = free
+    out_argument(x)
+    assert 10 == x
+
+def out_argument2(x: 0, y):
+    assert y == 10
+
+def out_argument2(x: 1, y):
+    assert y == 20
+
+def dispatch_test4_out_argument_with_overloads():
+    x0 = free
+    out_argument2(x0, 10)
+    assert 0 == x0
+
+    x1 = free
+    out_argument2(x1, 20)
+    assert 1 == x1
+
+    y0 = free
+    out_argument2(0, y0)
+    assert 10 == y0
+
+    y1 = free
+    out_argument2(1, y1)
+    assert 20 == y1
+
 def main():
     dispatch_test0_nondeterministic_function()
     dispatch_test1_predicate_dispatch()
     dispatch_test2_match_in_arg_list()
+    dispatch_test3_out_argument()
+    dispatch_test4_out_argument_with_overloads()
+
     print(1)
